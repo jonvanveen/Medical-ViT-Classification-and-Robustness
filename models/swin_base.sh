4fbf57f31667a1f2da0ff06f1a18612ecd70a310
Jon Van Veen <56141068+jonvanveen@users.noreply.github.com>
2022-08-18 17:30:52 -0500

    Update swin_base.sh

--- models/swin_base.sh
+++ models/swin_base.sh
@@ -1,12 +1,15 @@
+# Shell script to execute baseline Swin-T code on the WACC Euler compute cluster
+# Specifies memory, runtime, and GPUs for job
+
 #!/usr/bin/env zsh
 #SBATCH --job-name=swin_chxray_base_model
 #SBATCH --partition=wacc
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
 #SBATCH --time=0-24:00:00
 #SBATCH --output="benchmark_1_core_1_gpu-%j.txt"
-#SBATCH -G 4
+#SBATCH -G 8
 
 cd $SLURM_SUBMIT_DIR
 
@@ -14,17 +17,5 @@ module load anaconda/full
 bootstrap_conda
 conda activate swin
 
-#conda update -n base -c defaults conda
-#conda install pytorch==1.8.0 torchvision==0.9.0 cudatoolkit=10.2 -c pytorch
-#pip install timm==0.4.12 opencv-python==4.4.0.46 termcolor==1.1.0 yacs==0.1.8
-
-#cd kernels/window_process
-#python setup.py install #--user
-
 # Pretrained swin
-python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345 main.py --cfg configs/swin/swin_tiny_patch4_window7_224.yaml --resume ~/Models/swin_chxray_base_output/swin_tiny_patch4_window7_224/default/ckpt_epoch_40.pth --data-path ~/Data/chxray/imagenet --batch-size 128  --opts TRAIN.EPOCHS 200 TRAIN.WARMUP_EPOCHS 10 TRAIN.BASE_LR 0.0002 --output ~/Models/swin_chxray_base_output
-
-# NOT Swin train from scratch
-#python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345 main.py --eval --cfg configs/swin/swin_tiny_patch4_window7_224.yaml --resume swin_tiny_patch4_window7_224.pth --data-path ~/Data/chxray/imagenet --batch-size 128  --opts TRAIN.EPOCHS 200 TRAIN.WARMUP_EPOCHS 10 TRAIN.BASE_LR 0.0002 --output ~/Models/swin_chxray_base_output
-
-python test.py
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345 main.py --cfg configs/swin/swin_tiny_patch4_window7_224.yaml --resume ~/Models/swin_chxray_base_output/swin_tiny_patch4_window7_224/default/ckpt_epoch_40.pth --data-path ~/Data/chxray/imagenet --batch-size 128  --opts TRAIN.EPOCHS 200 TRAIN.WARMUP_EPOCHS 10 TRAIN.BASE_LR 0.0002 --output ~/Models/swin_chxray_base_output
