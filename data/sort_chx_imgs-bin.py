34cb4b57f7f353a9cc53b73f002b5f5e23005ad3
Jon Van Veen <56141068+jonvanveen@users.noreply.github.com>
2022-08-18 18:10:38 -0500

    Update sort_chx_imgs-bin.py

--- data/sort_chx_imgs-bin.py
+++ data/sort_chx_imgs-bin.py
@@ -1,3 +1,10 @@
+"""
+Code to sort two classes for NIH Chest x-ray images for binary classification.
+I coded this script as a simpler classification problem as a backup in case
+the multi-class NIH dataset was not suitable for Swin-T, but this sorting
+ultimately went unused.
+"""
+
 import os
 import numpy as np
 import csv
@@ -6,6 +13,7 @@ import shutil
 with open('data_entries.csv', mode='r') as csv_file:
     data_entries = csv.reader(csv_file,delimiter=',')
     rows = list(data_entries)
+    split = len(rows)*0.8
 
 raw_imgs = os.listdir('raw_imgs')
 label_list = ['Pneumonia','No Finding']
@@ -14,13 +22,13 @@ file_count=0
 for row in rows: # training data
     labelnum = 1
     for label in label_list:
-
-        if file_count<89698:
+        
+        # 80-20 split
+        if file_count<split:
             class_path = 'imagenet/train/class'+str(labelnum)
             if label in row[1]:
-                #shutil.copy('raw_imgs/'+row[0], class_path)
-                1+1
-                #print("Sorted file #",file_count,", ",row[0]," into ",class_path)
+                shutil.copy('raw_imgs/'+row[0], class_path)
+                print("Sorted file #",file_count,", ",row[0]," into ",class_path)
         else:
             class_path = 'imagenet/val/class'+str(labelnum)
             if label in row[1]:
